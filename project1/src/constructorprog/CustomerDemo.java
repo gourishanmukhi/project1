3c9e5db59845f76dbf09cb486c4d33c27b1eb60b
lenovo <lenovo@DESKTOP-JS6VV19>
2023-10-03 14:41:55 +0530

    third commit

---
+++ project1/src/constructorprog/CustomerDemo.java
@@ -0,0 +1,24 @@
+package constructorprog;
+import java.util.Scanner;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+       String name,city;
+       int id;
+       System.out.println("enter customer id");
+       id=sc.nextInt();
+       System.out.println("enter name");
+       name=sc.nextLine();
+       System.out.println("enter city");
+       city=sc.nextLine();
+       Customer c1 = new Customer();
+       c1.setCustomerId(id);
+       c1.setCustomerName(name);
+       c1.setCustomerCity(city);
+       System.out.println(c1);
+	}
+}
+
