67b978586bc64e54e26ad4abc8aed95dbe5253df
lenovo <lenovo@DESKTOP-JS6VV19>
2023-10-25 12:03:33 +0530

    thread commit

---
+++ project1/src/com/tns/thread/CreatingThreadFirst.java
@@ -0,0 +1,19 @@
+package com.tns.thread;
+
+public class CreatingThreadFirst extends Thread {
+
+	public void run(){
+		System.out.println("Hello");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CreatingThreadFirst ob =new CreatingThreadFirst();
+		ob.start();
+
+	}
+
+	}
+
+
