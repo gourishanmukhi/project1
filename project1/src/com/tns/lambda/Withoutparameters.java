50e7e38ac38bbe4409bae75c4e06ca3ba3a06382
lenovo <lenovo@DESKTOP-JS6VV19>
2023-10-29 10:39:22 +0530

    lambda commit

---
+++ project1/src/com/tns/lambda/Withoutparameters.java
@@ -0,0 +1,9 @@
+package com.tns.lambda;
+
+public class Withoutparameters {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Message m = ()->{return "hello";};
+		System.out.println(m.greet());	
+		}
+}
